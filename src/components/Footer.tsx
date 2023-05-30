aa33beb10941cd1b360b647f74825b03f75885e3
Natsune Mochizuki <me@natsuneko.cat>
2023-05-30 20:01:59 +0900

    feat: wide header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="px-4 pb-16">
-      <div className="container mx-auto border-t border-neutral-600 pt-4 text-sm ">
+    <footer className="border-t border-neutral-600 px-8 pb-16">
+      <div className="pt-4 text-sm">
         <div className="flex items-center opacity-70">
           <p>&copy; {new Date().getFullYear()} Natsuneko.</p>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const Header: React.FC = () => {
   return (
     <header className="sticky top-0 z-10 h-16 border-b border-neutral-600">
-      <div className="h-full bg-neutral-950 px-4">
-        <div className="container mx-auto flex h-full items-center justify-between">
+      <div className="h-full bg-neutral-950 px-8">
+        <div className=" flex h-full items-center justify-between">
           <a href="/" className="flex  items-center gap-x-4">
             <Image src="/logo.png" alt="logo" width={36} height={36} />
             <span>Natsuneko Docs</span>
