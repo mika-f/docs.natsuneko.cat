0b76ba70bb9deeafe496190221a081d3a02ff468
Natsune Mochizuki <me@natsuneko.cat>
2023-05-31 20:05:25 +0900

    feat: support important blockquote

--- src/components/Markdown/Blockquote.tsx
+++ src/components/Markdown/Blockquote.tsx
@@ -11,7 +11,7 @@ const Blockquote: React.FC<Props> = ({ className, ...props }) => {
     const { children, ...rest } = props;
 
     switch (type) {
-      case "note":
+      case "important":
         return (
           <blockquote
             className={merge(
@@ -21,6 +21,23 @@ const Blockquote: React.FC<Props> = ({ className, ...props }) => {
             {...rest}
           >
             <p className="mb-1 flex items-center text-xl text-sky-300">
+              <i className="fa-regular fa-info-circle" />
+              <span className="ml-2 text-lg">Important</span>
+            </p>
+            <div>{children}</div>
+          </blockquote>
+        );
+
+      case "note":
+        return (
+          <blockquote
+            className={merge(
+              "my-4 border border-indigo-600 bg-indigo-900 px-4 py-2",
+              className
+            )}
+            {...rest}
+          >
+            <p className="mb-1 flex items-center text-xl text-indigo-300">
               <i className="fa-regular fa-note" />
               <span className="ml-2 text-lg">Note</span>
             </p>
