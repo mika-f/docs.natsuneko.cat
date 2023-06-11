f85ce85940859aae92ff9e9b0b8df05478efb5ec
Natsune Mochizuki <me@natsuneko.cat>
2023-06-11 20:14:20 +0900

    fix: fix wrapping

--- src/components/Markdown/Code.tsx
+++ src/components/Markdown/Code.tsx
@@ -9,10 +9,9 @@ type Props = {
 const Code: React.FC<Props> = ({ className, ...props }) => (
   <code
     className={merge(
-      "relative mx-0.5 whitespace-break-spaces rounded bg-neutral-500 bg-opacity-25 p-1 font-mono text-sm text-neutral-300",
+      "relative mx-0.5 whitespace-break-spaces break-words rounded bg-neutral-500 bg-opacity-25 p-1 font-mono text-sm text-neutral-300",
       props["data-theme"] &&
         "mx-0 grid whitespace-pre break-words bg-transparent",
-      // "group-[.code]:mx-0 group-[.code]:grid group-[.code]:whitespace-pre group-[.code]:break-words group-[.code]:bg-transparent group-[.code]:px-4",
       className
     )}
     {...props}
