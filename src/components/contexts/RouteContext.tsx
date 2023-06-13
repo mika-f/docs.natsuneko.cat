75bc5f882f9e58dc25879be0ccdab204f85f08c5
Natsune Mochizuki <me@natsuneko.cat>
2023-06-13 09:02:37 +0900

    fix: fix route context assign

--- src/components/contexts/RouteContext.tsx
+++ src/components/contexts/RouteContext.tsx
@@ -25,6 +25,59 @@ const RouteContext = createServerContext<RouteContextProps | null>(
   null
 );
 
+const build = (context: RouteContextProps, ctx: ModifiedPathParams): string => {
+  const { language, product, version, rest, absolute, relative } = {
+    ...context,
+    ...ctx,
+  };
+
+  const url: string[] = [language];
+
+  if (product) {
+    url.push(product);
+  }
+
+  if (version) {
+    url.push(version);
+  }
+
+  if (rest) {
+    url.push(...rest);
+  }
+
+  const path = `/${url.join("/")}/`;
+  const assign =
+    absolute ?? (relative?.startsWith("/") ? `.${relative}` : relative);
+  if (assign) {
+    return new URL(assign, `https://docs.natsuneko.cat${path}`).pathname;
+  }
+
+  return path;
+};
+
+const assign = (
+  defaults: RouteContextProps,
+  ctx: ModifiedPathParams
+): RouteContextT => {
+  const url = build(defaults, ctx);
+  const paths = url.split("/").filter((w) => !!w);
+  const context: RouteContextProps = {
+    language: paths[0],
+    product: paths[1] ?? null,
+    version: paths.length >= 3 && isValidVersion(paths[2]) ? paths[2] : null,
+    rest:
+      paths.length >= 3 && isValidVersion(paths[2])
+        ? paths.slice(3)
+        : paths.slice(2),
+  };
+
+  return {
+    ...context,
+    assign: (ctx) => assign(context, ctx),
+    build: (ctx) => build(context, { ...ctx, absolute: url }),
+  };
+};
+
 const useRouteContext = (): RouteContextT => {
   const context = useContext(RouteContext);
 
@@ -34,57 +87,10 @@ const useRouteContext = (): RouteContextT => {
     );
   }
 
-  const build = (ctx: ModifiedPathParams): string => {
-    const { language, product, version, rest, absolute, relative } = {
-      ...context,
-      ...ctx,
-    };
-
-    const url: string[] = [language];
-
-    if (product) {
-      url.push(product);
-    }
-
-    if (version) {
-      url.push(version);
-    }
-
-    if (rest) {
-      url.push(...rest);
-    }
-
-    const path = `/${url.join("/")}/`;
-    const assign =
-      absolute ?? (relative?.startsWith("/") ? `.${relative}` : relative);
-    if (assign) {
-      return new URL(assign, `https://docs.natsuneko.cat${path}`).pathname;
-    }
-
-    return path;
-  };
-
-  const assign = (ctx: ModifiedPathParams): RouteContextT => {
-    const url = build(ctx);
-    const paths = url.split("/").filter((w) => !!w);
-
-    return {
-      language: paths[0],
-      product: paths[1] ?? null,
-      version: paths.length >= 3 && isValidVersion(paths[2]) ? paths[2] : null,
-      rest:
-        paths.length >= 3 && isValidVersion(paths[2])
-          ? paths.slice(3)
-          : paths.slice(2),
-      assign,
-      build: (ctx) => build({ ...ctx, absolute: url }),
-    };
-  };
-
   return {
     ...context,
-    assign,
-    build,
+    assign: (ctx: ModifiedPathParams) => assign(context, ctx),
+    build: (ctx: ModifiedPathParams) => build(context, ctx),
   };
 };
 
