cc5f854b96a4d75ce4bfb41ac9dd5efbdd112d75
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-10-27 19:15:05 +0900

    feat: add logo and link it

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+
 const Footer: React.FC = () => {
   return (
     <footer className="border-t border-neutral-600 pb-16">
       <div className="container mx-auto pt-4 text-sm">
-        <div className="mx-4 flex items-center opacity-70 lg:mx-0">
-          <p>&copy; {new Date().getFullYear()} Natsuneko.</p>
+        <div className="mx-4 flex items-center lg:mx-0">
+          <div className="flex flex-col gap-y-4">
+            <div className="text-base">Powered by</div>
+            <a className="block" target="_blank" href="https://natsuneko.com">
+              <Image
+                src="/natsuneko-laboratory.png"
+                width={1217 / 6}
+                height={358 / 6}
+                alt="logo"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
