d93cd77016e07f0f6e3aabd16667589540cfa607
MOCHIZUKI Natsune <me@natsuneko.cat>
2025-09-01 22:16:46 +0900

    chore: spacing

--- src/components/markdown/Paragraph.tsx
+++ src/components/markdown/Paragraph.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const Paragraph = ({ children }: Props) => {
   return (
-    <p className="leading-7 [&:not(:first-child)]:mt-6 mb-2 group-[.admonition]:mt-0! group-[.admonition]:first:hidden">
+    <p className="leading-7 [&:not(:first-child)]:mt-6 first:mt-2 mb-2 group-[.admonition]:mt-0! group-[.admonition]:first:hidden">
       {children}
     </p>
   );
